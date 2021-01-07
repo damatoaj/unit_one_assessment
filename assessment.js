bbe5ab9204a1557fdb6f56520395467e4a59d24e
damatoaj <mr.arthurdamato@gmail.com>
2021-01-07 12:04:33 -0800

    third attempt

--- assessment.js
+++ assessment.js
@@ -1,6 +1,8 @@
 let input = parseInt(document.getElementById('myNumber').innerHTML);
 let solution = parseInt(document.getElementById('solution').innerHTML);
 
+
+
 function myAddition() {
     var x = parseInt(document.getElementById('myNumber').value);
     var n = solution + x;
@@ -8,16 +10,16 @@ function myAddition() {
     document.getElementById('solution').innerHTML = n;
     console.log(n)
 }
-
+document.getElementById('addition').addEventListener('click', myAddition())
 
 function mySubtraction() {
     var y = parseInt(document.getElementById('myNumber').value);
     var o = solution - y;
-    o--
     document.getElementById('solution').innerHTML = o;
 
     console.log(o)
 }
+document.getElementById('subtraction').addEventListener('click', mySubtraction())
 
 
 // function changeColor() {
