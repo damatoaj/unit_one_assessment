a34076e98a4bb9d23759e2b7ec9744355fcdaefd
damatoaj <mr.arthurdamato@gmail.com>
2021-01-07 18:52:48 -0800

    finished javascripts

--- assessment.js
+++ assessment.js
@@ -1,28 +1,66 @@
-let input = parseInt(document.getElementById('myNumber').innerHTML);
-let solution = parseInt(document.getElementById('solution').innerHTML);
+const display = document.getElementById('solution');
+const input = document.getElementById('myNumber');
+const plus = document.getElementById('addition');
+const minus = document.getElementById('subtraction');
 
+let num = 0;
 
-
-function myAddition() {
-    var x = parseInt(document.getElementById('myNumber').value);
-    var n = solution + x;
-    n++
-    document.getElementById('solution').innerHTML = n;
-    console.log(n)
+function addNum(e) {
+    e.preventDefault();
+    num += parseInt(input.value);
+    display.innerText = num;
+    if (num >= 0) {
+        display.style.color = 'black';
+    }
 }
-document.getElementById('addition').addEventListener('click', myAddition())
 
-function mySubtraction() {
-    var y = parseInt(document.getElementById('myNumber').value);
-    var o = solution - y;
-    document.getElementById('solution').innerHTML = o;
 
-    console.log(o)
+
+function subtractNum(e) {
+    e.preventDefault();
+    num -= parseInt(input.value);
+    display.innerText = num;
+    if(num < 0) {
+        display.style.color = 'red';
+    } else {
+        display.style.color = 'black';
+    }
 }
-document.getElementById('subtraction').addEventListener('click', mySubtraction())
+
+plus.addEventListener('click', (e)=> addNum(e))
+minus.addEventListener('click', (e)=> subtractNum(e))
 
 
-// function changeColor() {
-//     document.getElementsByClassName('buttons').style.background-color = 'white';
-//     document.getElementsByClassName('buttons').style.color = 'grey';
-// }
+function changePlus(e) {
+    e.preventDefault();
+    if ('mouseover') {
+    plus.style.backgroundColor = 'white';
+    plus.style.color = 'grey';
+    } else (plus.style.backgroundColor == 'grey' &&
+            plus.style.color == 'white')
+}  
+
+function changeMinus(e) {
+    e.preventDefault();
+    minus.style.backgroundColor = 'white';
+    minus.style.color = 'grey';
+}
+
+
+plus.addEventListener('mouseover', (e)=> changePlus(e))
+minus.addEventListener('mouseover', (e)=> changeMinus(e))
+
+function returnPlus(e) {
+    e.preventDefault();
+    plus.style.backgroundColor = 'grey';
+    plus.style.color = 'white';
+}
+
+function returnMinus(e) {
+    e.preventDefault();
+    minus.style.backgroundColor = 'grey';
+    minus.style.color = 'white';
+}
+
+plus.addEventListener('mouseout', (e)=> returnPlus(e))
+minus.addEventListener('mouseout', (e)=> returnMinus(e))
